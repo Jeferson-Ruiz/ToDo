2bd4cda74d8a6f395f654495e5f86edb6d9e469e
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-03-19 20:58:12 -0500

    feat(repository) crear interfaz y metodos para la entidad Category

---
+++ src/main/java/com/jr/todo/repository/CategoryRepository.java
@@ -0,0 +1,17 @@
+package com.jr.todo.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.jr.todo.entity.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+  @Query("SELECT Categority c FROM Category WHERE c.name = :name")
+  Optional<Category> findByName(@Param("name") String name);
+
+  @Query("SELECT COUNT(c)>0 FROM Category c WHERE c.name =:name")
+  boolean existByName(@Param("name") String name);
+}
