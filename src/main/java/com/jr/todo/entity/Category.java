2ad1182335ebaf78a693f9b56161749953373ac7
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-03-19 20:56:46 -0500

    feat(entity) crear entidad Category, relacion con M a 1 con Task

--- src/main/java/com/jr/todo/entity/Category.java
+++ src/main/java/com/jr/todo/entity/Category.java
@@ -1,30 +1,39 @@
 package com.jr.todo.entity;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
+@Entity
 @Table(name = "categoria")
-public class Categority {
+public class Category {
 
   @Id
+  @Column(name = "cat_id", unique = true, nullable = false)
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  private Long categoryId;
 
   @Column(name = "cat_nombre", length = 40, nullable = false, unique = true)
   private String name;
 
   private String description;
+
+  private LocalDateTime dateCreation;
+
+  @OneToMany(mappedBy = "category")
+  private List<Task> task;
 }
