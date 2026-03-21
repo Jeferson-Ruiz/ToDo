9d271eff3e00c5842bbd41223d7eab014974fa22
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-03-21 12:14:47 -0500

    feat(entity) crear entidad de categoria

--- src/main/java/com/jr/todo/entity/Category.java
+++ src/main/java/com/jr/todo/entity/Category.java
@@ -1,7 +1,10 @@
 package com.jr.todo.entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -27,13 +30,13 @@ public class Category {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long categoryId;
 
-  @Column(name = "cat_nombre", length = 40, nullable = false, unique = true)
+  @Column(name = "cat_nombre", length = 40, unique = true)
   private String name;
 
   private String description;
 
   private LocalDateTime dateCreation;
 
-  @OneToMany(mappedBy = "category")
-  private List<Task> task;
+  @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+  private List<Task> task = new ArrayList<>();
 }
