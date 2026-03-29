548e759fd714f77f5b2ad0feef8ba1fed5ee276a
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-03-29 15:47:41 -0500

    feat(service) crear clase y metodos en service para User

---
+++ src/main/java/com/jr/todo/service/UserService.java
@@ -0,0 +1,51 @@
+package com.jr.todo.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.jr.todo.dto.UserDto;
+import com.jr.todo.entity.User;
+import com.jr.todo.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class UserService {
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
+  }
+
+  public UserDto createUser(UserDto userDto) {
+    existUser(userDto.username());
+
+    User user = userDto.toEntity();
+    user.setPassword(passwordEncoder.encode(userDto.password()));
+    return UserDto.toDto(userRepository.save(user));
+  }
+
+  public void updatePasswod(Long id, String oldPassword, String newPassword) {
+    User user = findUserById(id);
+
+    if (!passwordEncoder.encode(oldPassword).equals(user.getPassword())) {
+      throw new IllegalArgumentException("Error de validacion de contraseña");
+    }
+    user.setPassword(passwordEncoder.encode(newPassword));
+    userRepository.save(user);
+  }
+
+  // helpers
+  private void existUser(String name) {
+    if (userRepository.existsByName(name)) {
+      throw new IllegalArgumentException("username ya registrado");
+    }
+  }
+
+  private User findUserById(Long id) {
+    User user = userRepository.findById(id)
+        .orElseThrow(() -> new EntityNotFoundException("Usuario no encontrado"));
+    return user;
+  }
+}
