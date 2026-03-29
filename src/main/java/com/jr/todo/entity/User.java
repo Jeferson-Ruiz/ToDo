0961c8a6676d00eeaa7654811726ded93f06805f
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-03-29 15:45:22 -0500

    feat(entity) crear entidad User con variables

---
+++ src/main/java/com/jr/todo/entity/User.java
@@ -0,0 +1,38 @@
+package com.jr.todo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "usuarios")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "usr_id", unique = true)
+  private Long id;
+
+  @Column(name = "usr_name", nullable = false)
+  private String name;
+
+  @Column(name = "usr_username", nullable = false, unique = true)
+  private String username;
+
+  @Column(name = "usr_email", nullable = false, unique = true)
+  private String email;
+
+  @Column(name = "usr_password", nullable = false)
+  private String password;
+}
