da2f4a35925fa28c441bcf9e24d4ff042929927a
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-03-29 21:19:44 -0500

    refactor(controller) implementar Dto y RequestBody para actualizar password

--- src/main/java/com/jr/todo/controller/UserController.java
+++ src/main/java/com/jr/todo/controller/UserController.java
@@ -2,15 +2,16 @@ package com.jr.todo.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.jr.todo.dto.DataDto;
+import org.springframework.web.bind.annotation.RestController;
+import com.jr.todo.dto.PasswordUpdateDto;
 import com.jr.todo.dto.UserDto;
 import com.jr.todo.service.UserService;
 
-@RequestMapping
+@RestController
+@RequestMapping("/user")
 public class UserController {
   private final UserService userService;
 
@@ -18,16 +19,15 @@ public class UserController {
     this.userService = userService;
   }
 
-  @PostMapping("/creare")
+  @PostMapping("/create")
   public ResponseEntity<?> userCreate(@RequestBody UserDto userRequest) {
     UserDto userDto = userService.createUser(userRequest);
     return ResponseEntity.ok(userDto);
   }
 
-  @PatchMapping("/update-password/{id}")
-  public ResponseEntity<?> updatePassword(@PathVariable long id, @RequestBody DataDto oldPasword,
-      @RequestBody DataDto newPassword) {
-    userService.updatePasswod(id, oldPasword.data(), newPassword.data());
+  @PatchMapping("/update-password")
+  public ResponseEntity<?> updatePassword(@RequestBody PasswordUpdateDto data) {
+    userService.updatePasswod(data.id(), data.oldPassword(), data.newPassword());
     return ResponseEntity.noContent().build();
   }
 
