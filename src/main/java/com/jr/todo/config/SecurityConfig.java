7e797a72a0f3bdf2033ea78e5d7771979dba6e30
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-03-29 20:52:34 -0500

    feat(config) crear class para desacticar el csrf

--- src/main/java/com/jr/todo/config/SecurityConfig.java
+++ src/main/java/com/jr/todo/config/SecurityConfig.java
@@ -1,16 +1,32 @@
 package com.jr.todo.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Component
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Configuration
+  @EnableWebSecurity
+  public class CsrfSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+      http
+          .csrf((csrf) -> csrf.disable());
+      return http.build();
+    }
   }
 
 }
