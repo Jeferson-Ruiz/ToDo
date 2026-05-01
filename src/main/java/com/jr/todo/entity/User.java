03c7d07a8cb7ce97f23be6791a735e74fa6f81b2
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-05-01 18:16:55 -0500

    feat(entity): implementar UserDetails para metods de validacion

--- src/main/java/com/jr/todo/entity/User.java
+++ src/main/java/com/jr/todo/entity/User.java
@@ -1,8 +1,16 @@
 package com.jr.todo.entity;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import com.jr.todo.entity.enums.Role;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -18,7 +26,7 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name = "usuarios")
-public class User {
+public class User implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -43,7 +51,36 @@ public class User {
   @Column(name = "usr_activo")
   private boolean enable;
 
+  @Column(name = "usr_role")
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
   @Column(name = "_usr_fecha_registro")
   private LocalDate registrationDte;
 
+  // Metodos de interaz UserDetails
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority(role.name()));
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
