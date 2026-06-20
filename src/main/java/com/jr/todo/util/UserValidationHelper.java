dd24595603262034e9f813ace4f1762e6d742a62
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-06-20 11:56:22 -0500

    feat: crear clase helper con metodos reutilizables

---
+++ src/main/java/com/jr/todo/util/UserValidationHelper.java
@@ -0,0 +1,33 @@
+package com.jr.todo.util;
+
+import org.springframework.stereotype.Component;
+import com.jr.todo.entity.User;
+import com.jr.todo.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+
+@Component
+public class UserValidationHelper {
+    private final UserRepository userRepository;
+
+    public UserValidationHelper(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void validateUsername(String name) {
+        if (userRepository.existsByName(name)) {
+            throw new IllegalArgumentException("username ya registrado");
+        }
+    }
+
+    public void validateEmail(String email) {
+        if (userRepository.existByEmail(email)) {
+            throw new IllegalArgumentException("username ya registrado");
+        }
+    }
+
+    public User findUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Usuario no encontrado"));
+        return user;
+    }
+}
