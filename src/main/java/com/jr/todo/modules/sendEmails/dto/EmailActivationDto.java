427b90be2910949e5eb408b69fb8e2ec4e13730b
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-06-27 09:22:42 -0500

    feat: agregar variable en el dto para agregar tema del correo

--- src/main/java/com/jr/todo/modules/sendEmails/dto/EmailActivationDto.java
+++ src/main/java/com/jr/todo/modules/sendEmails/dto/EmailActivationDto.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 @Setter
 public class EmailActivationDto {
     private String recipient;
+    private String subject;
     private String userName;
     private String activationUrl;
     private Long expirationHours;
