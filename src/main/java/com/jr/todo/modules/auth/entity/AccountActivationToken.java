ce27fe45a7a08b5832f18ea5b477f4983b0685cf
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-06-29 11:11:06 -0500

    feat: crear entidad AccountActivationToken

---
+++ src/main/java/com/jr/todo/modules/auth/entity/AccountActivationToken.java
@@ -0,0 +1,41 @@
+package com.jr.todo.modules.auth.entity;
+
+import java.time.LocalDateTime;
+import com.jr.todo.modules.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "account_activation_tokens")
+public class AccountActivationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    private boolean used;
+}
