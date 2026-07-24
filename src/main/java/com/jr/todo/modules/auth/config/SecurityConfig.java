18d4db3cb40c78d4668b1997a785a8214e5b8c52
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-07-23 20:19:03 -0500

    feat: crear ruta filtro del rol a las rutas de admin

--- src/main/java/com/jr/todo/modules/auth/config/SecurityConfig.java
+++ src/main/java/com/jr/todo/modules/auth/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.jr.todo.modules.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,7 +28,7 @@ public class SecurityConfig {
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(authRequest -> authRequest
             .requestMatchers("/auth/**", "/recovery/**").permitAll()
-            .requestMatchers(HttpMethod.POST, "/user/create").hasRole("ADMIN")
+            .requestMatchers("/admin/**").hasRole("ADMIN")
             .anyRequest().authenticated())
         .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authenticationProvider(authProvider)
