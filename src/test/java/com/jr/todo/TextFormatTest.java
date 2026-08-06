e2b8cde06f6aab889ee95545d7d5f33271cdb0fb
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-08-06 15:12:42 -0500

    feat: crear test unitarios par la clase util TextFormat

---
+++ src/test/java/com/jr/todo/TextFormatTest.java
@@ -0,0 +1,40 @@
+package com.jr.todo;
+
+import org.junit.jupiter.api.Test;
+import com.jr.todo.util.TextFormat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextFormatTest {
+
+	@Test
+	void testNameFormat() {
+		String name = "pedro pascal";
+		String result = TextFormat.nameFormat(name);
+		assertEquals("Pedro pascal", result);
+		assertNotNull(result);
+	}
+
+	@Test
+	void testValidaTextNull() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			TextFormat.validaTextNull(null);
+		});
+	}
+
+	@Test
+	void testValidaTextBlank() {
+		String text = " ";
+		assertThrows(IllegalArgumentException.class, () -> {
+			TextFormat.validaTextNull(text);
+		});
+	}
+
+	@Test
+	void testNameFormatWithText() {
+		String text = "Texto";
+		String restulr = TextFormat.validaTextNull(text);
+
+		assertNotNull(restulr);
+		assertEquals("texto", restulr);
+	}
+}
