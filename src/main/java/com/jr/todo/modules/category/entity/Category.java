f30c7d6e1b7dc6aaa7805af7880e2c7f6b41ba9d
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-08-29 10:45:56 -0500

    feat: crear relacion con M a 1 con User

--- src/main/java/com/jr/todo/modules/category/entity/Category.java
+++ src/main/java/com/jr/todo/modules/category/entity/Category.java
@@ -4,14 +4,19 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import com.jr.todo.modules.task.entity.Task;
+import com.jr.todo.modules.user.entity.User;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,7 +27,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "categoria")
+@Table(name = "categoria", uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "cat_nombre"}))
 public class Category {
 
   @Id
@@ -30,12 +35,16 @@ public class Category {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long categoryId;
 
-  @Column(name = "cat_nombre", length = 40, unique = true)
+  @Column(name = "cat_nombre", length = 40)
   private String name;
 
   private String description;
 
   private LocalDateTime dateCreation;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
 
   @OneToMany(mappedBy = "category", cascade = CascadeType.PERSIST)
   private List<Task> task = new ArrayList<>();
