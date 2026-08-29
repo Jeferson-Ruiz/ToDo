ff463add052650efaa66bf2eb50ab6297183f1a6
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-08-28 19:24:22 -0500

    fix: prefijo Role en la clase User para que el hastRole funcione

--- src/main/java/com/jr/todo/modules/user/entity/User.java
+++ src/main/java/com/jr/todo/modules/user/entity/User.java
@@ -72,7 +72,7 @@ public class User implements UserDetails {
   // Metodos de interaz UserDetails
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of(new SimpleGrantedAuthority(role.name()));
+    return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
   }
 
   @Override
