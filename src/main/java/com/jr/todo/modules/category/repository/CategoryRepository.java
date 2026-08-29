b7a3a15d8109a6d04ce335357a88badaf0042e3c
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-08-29 10:58:31 -0500

    feat: crear metodos en el repositorio que implementan la validacion por User

--- src/main/java/com/jr/todo/modules/category/repository/CategoryRepository.java
+++ src/main/java/com/jr/todo/modules/category/repository/CategoryRepository.java
@@ -1,5 +1,6 @@
 package com.jr.todo.modules.category.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,9 +8,14 @@ import org.springframework.data.repository.query.Param;
 import com.jr.todo.modules.category.entity.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-  @Query("SELECT c FROM Category c WHERE c.name = :name")
-  Optional<Category> findByName(@Param("name") String name);
+  List<Category> findAllByUserId(Long userId);
 
-  @Query("SELECT COUNT(c)>0 FROM Category c WHERE c.name =:name")
-  boolean existByName(@Param("name") String name);
+  @Query("SELECT c FROM Category c WHERE c.categoryId = :id AND c.user.id = :userId")
+  Optional<Category> findByCategoryIdAndUserId(@Param("id") Long id, @Param("userId") Long userId);
+
+  @Query("SELECT c FROM Category c WHERE c.name = :name AND c.user.id = :userId")
+  Optional<Category> findByNameAndUserId(@Param("name") String name, @Param("userId") Long userId);
+
+  @Query("SELECT COUNT(c)>0 FROM Category c WHERE c.name = :name AND c.user.id = :userId")
+  boolean existsByNameAndUserId(@Param("name") String name, @Param("userId") Long userId);
 }
