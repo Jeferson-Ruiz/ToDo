192f5d668af43359c8b4f73975ac5a8ef0acaff1
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-08-29 09:59:44 -0500

    fix: corregir nombre de la excepcion en el test

--- src/main/java/com/jr/todo/util/UserValidationHelper.java
+++ src/main/java/com/jr/todo/util/UserValidationHelper.java
@@ -25,7 +25,7 @@ public class UserValidationHelper {
 
     public void isEnabled(String email) {
         if (!userRepository.isUserEnabled(email)) {
-            throw new IllegalArgumentException("Usuario desactivado, validar mediante emial");
+            throw new IllegalAccessError("Usuario desactivado, validar mediante emial");
         }
     }
 }
