64fe38846aa78a3ed8674488bd81f2a31a342c21
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-08-29 11:12:30 -0500

    feat: crear relacion  M a 1 con la entidad User

--- src/main/java/com/jr/todo/modules/task/entity/Task.java
+++ src/main/java/com/jr/todo/modules/task/entity/Task.java
@@ -4,14 +4,17 @@ import java.time.LocalDateTime;
 import com.jr.todo.modules.category.entity.Category;
 import com.jr.todo.modules.task.enums.Priority;
 import com.jr.todo.modules.task.enums.Status;
+import com.jr.todo.modules.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,7 +25,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "tareas")
+@Table(name = "tareas", uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "name"}))
 public class Task {
 
   @Id
@@ -30,7 +33,7 @@ public class Task {
   @Column(name = "task_id", unique = true, nullable = false)
   private Long taskId;
 
-  @Column(nullable = false, length = 50, unique = true)
+  @Column(nullable = false, length = 50)
   private String name;
 
   @Column(nullable = false, length = 200)
@@ -48,4 +51,8 @@ public class Task {
   @ManyToOne(optional = true)
   @JoinColumn(name = "category_id")
   private Category category;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
 }
